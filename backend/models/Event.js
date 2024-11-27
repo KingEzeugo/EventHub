2660b0300efc5424466016dbd3e1e672093713c0
Pablo Lasarte <pl8n4@umsystem.edu>
2024-11-27 12:34:05 -0600

    Expanded model for event

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 
 const EventSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    date: { type: String, required: true },
-    address: { type: String, required: true },
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    date: {type: Date, required: true},
+    address: {
+        street: {
+            type: String,
+            required: true,
+        },
+        city: {type: String, required: true},
+        state: {type: String,required: true},
+        zip: {type: String, required: true},
+        country: {type: String, required: true},
+    },
 });
 
 module.exports = mongoose.model('Event', EventSchema);
