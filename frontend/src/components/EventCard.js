4854c49bc4e50b9f53933af3e226f486e97a344c
Pablo Lasarte <pl8n4@umsystem.edu>
2024-12-05 15:46:55 -0600

    Updated EventsCard.js to accurately display data

--- frontend/src/components/EventCard.js
+++ frontend/src/components/EventCard.js
@@ -5,10 +5,13 @@ const EventCard = ({ event }) => {
         <div className="event-card">
             <h3>{event.title}</h3>
             <p>{event.description}</p>
-            <p className="event-date">Date: {event.date}</p>
-            <p className="event-address">Address: {event.address}</p>
+            <p className="event-date">Date: {new Date(event.date).toLocaleDateString()}</p>
+            <p className="event-address">
+                Address: {event.address.street}, {event.address.city}, {event.address.state}, {event.address.zip}, {event.address.country}
+            </p>
         </div>
     );
 };
+
 
 export default EventCard;
